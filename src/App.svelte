097bdbd2d865cd4e81ae91c8bbeaac43a2738e5f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-05-26 12:18:22 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import _ from 'lodash';
+  import DateDisplay from './DateDisplay';
+
+  let name = 'Snowpack';
+</script>
+
+<h1>Hello, {_.startCase(name)}!</h1>
+
+<label>
+  Name
+  <input bind:value={name}>
+</label>
+
+<DateDisplay />
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
